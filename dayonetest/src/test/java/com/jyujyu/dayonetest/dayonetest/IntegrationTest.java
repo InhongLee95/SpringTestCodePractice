31390b64fd64c44e6c817af9c4a47db80e3ffe3b
leeinhong95 <leeinhong9512@gmail.com>
2024-05-01 00:08:14 +0900

    Redis get set 테스트 코드 구현

--- dayonetest/src/test/java/com/jyujyu/dayonetest/dayonetest/IntegrationTest.java
+++ dayonetest/src/test/java/com/jyujyu/dayonetest/dayonetest/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jyujyu.dayonetest.dayonetest;
 
+import com.redis.testcontainers.RedisContainer;
 import jakarta.transaction.Transactional;
 import org.junit.Ignore;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,7 @@ import java.util.Map;
 @ContextConfiguration(initializers = IntegrationTest.IntegrationTestInitializer.class)
 public class IntegrationTest {
     static DockerComposeContainer rdbms;
+    static RedisContainer redis;
 
     static {
         //docker-compose에서 정의한 service Name을 입력한다.
@@ -38,8 +40,14 @@ public class IntegrationTest {
                                 .withStartupTimeout(Duration.ofSeconds(300))
                 );
 
-
+        //MySQL 컨테이너 실행 메서드
         rdbms.start();
+
+        //  사용할 Redis 이미지를 지정
+        redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6"));
+        redis.start();
+
+
     }
 
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -47,10 +55,22 @@ public class IntegrationTest {
         public void initialize(ConfigurableApplicationContext applicationContext) {
             Map<String, String> properties = new HashMap<>();
 
+
+            // 생성한 MySQL 컨테이너 접속
             var rdbmsHost = rdbms.getServiceHost("local-db", 3306);
             var rdbmsPort = rdbms.getServicePort("local-db", 3306);
 
+            // properties 설정 값 메모리에 저장
             properties.put("spring.datasource.url", "jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort + "/score");
+
+            // 생성한 redis 컨테이너 접속
+            var redisHost = redis.getHost();
+            var riedisPort = redis.getFirstMappedPort();
+
+
+            properties.put("spring.data.redis.host", redisHost);
+            properties.put("spring.data.redis.port", riedisPort.toString());
+
             TestPropertyValues.of(properties)
                     .applyTo(applicationContext);
         }
